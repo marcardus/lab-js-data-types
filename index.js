495597926d17f6ba141c4848ab426b3b78916189
marcardus <marcardus@gmail.com>
2025-03-29 12:13:43 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
@@ -23,10 +23,12 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-
+const camelTail1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const camelTail2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+const result = camelTail1 + camelTail2;
 // Print the cameLtaiL-formatted string
 
-
+console.log(result);
 
 
 /*******************************************
@@ -35,11 +37,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
 
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 
 /*******************************************
@@ -47,10 +49,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -74,3 +77,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+console.log("expression1:", expression1); // false
+console.log("expression2:", expression2); // true
+console.log("expression3:", expression3); // false
+console.log("expression4:", expression4); // true
+console.log("expression5:", expression5); // true
+console.log("expression6:", expression6); // false
+console.log("expression7:", expression7); // true
